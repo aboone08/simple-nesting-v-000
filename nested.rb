ac42962d200b11adc36dec2aac6ca90db8e47be4
Andranique Boone <andranique.boone@gmail.com>
2019-04-02 01:18:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,6 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 return programmer_hash[:dennis_ritchie][:languages]<<("Assembly")
-
+		 programmer_hash[:dennis_ritchie][:languages]<<("Assembly")
+		 return programmer_hash[:dennis_ritchie][:languages]
 end
