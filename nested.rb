06454d033e30602086ef8d7cb4579eb4894e16d1
Andranique Boone <andranique.boone@gmail.com>
2019-04-02 00:46:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,6 +79,10 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+				:yukihiro_matsumoto => {
+					:known_for => "Ruby",
+					:languages => ["LISP", "C"]
+				}
      }
 
 
